266db6baa179427766ce8da1312c885f0f765f50
krisluczka <krzysztofluczka@outlook.com>
2024-02-26 00:59:09 +0100

    Interface now allows to play against the engine.

--- Strongin/Strongin.cpp
+++ Strongin/Strongin.cpp
@@ -51,6 +51,30 @@ struct board {
         return true;
     }
 
+    /*
+        Forces a move
+        Returns false if the board is full
+    */
+    inline bool force_move( int_fast8_t column ) {
+        int_fast8_t og_column = column;
+        while ( height[column] == 6 ) {
+            column++;
+            if ( column > 6 ) column = 0;
+            if ( column == og_column ) return false;
+        }
+
+        if ( height[column] < 6 ) {
+            state[column][height[column]] = side;
+            height[column]++;
+            side = !side;
+        } else return false;
+
+        return true;
+    }
+
+    /*
+        Displays current board state
+    */
     void display() {
         for ( int_fast8_t row = 5; row >= 0; row-- ) {
             for ( int_fast8_t column = 0; column < 7; column++ ) {
@@ -63,14 +87,67 @@ struct board {
             }
             cout << endl;
         }
+        cout << "1 2 3 4 5 6 7" << endl;
     }
 };
 
 /*
     Checking whether board has a winning side
 */
-bool check_for_win( board* state, bool side ) {
-    return false;
+int_fast8_t check_for_win( board* state ) {
+    // vertical
+    for ( int_fast8_t j = 0; j < 7; j++ ) {
+        for ( int_fast8_t i = 0; i < 3; i++ ) {
+            if ( state->state[j][i] != -1
+                && state->state[j][i] == state->state[j][i + 1]
+                && state->state[j][i] == state->state[j][i + 2]
+                && state->state[j][i] == state->state[j][i + 3] ) {
+
+                return state->state[j][i];
+            }
+        }
+    }
+
+    // horizontal
+    for ( int_fast8_t i = 0; i < 6; i++ ) {
+        for ( int_fast8_t j = 0; j < 4; j++ ) {
+            if ( state->state[j][i] != -1
+                && state->state[j][i] == state->state[j + 1][i]
+                && state->state[j][i] == state->state[j + 2][i]
+                && state->state[j][i] == state->state[j + 3][i] ) {
+
+                return state->state[j][i];
+            }
+        }
+    }
+
+    // diagonal up
+    for ( int_fast8_t i = 0; i < 3; i++ ) {
+        for ( int_fast8_t j = 0; j < 4; j++ ) {
+            if ( state->state[j][i] != -1
+                && state->state[j][i] == state->state[j + 1][i + 1]
+                && state->state[j][i] == state->state[j + 2][i + 2]
+                && state->state[j][i] == state->state[j + 3][i + 3] ) {
+
+                return state->state[j][i];
+            }
+        }
+    }
+
+    // diagonal down
+    for ( int_fast8_t i = 3; i < 6; i++ ) {
+        for ( int_fast8_t j = 0; j < 4; j++ ) {
+            if ( state->state[j][i] != -1
+                && state->state[j][i] == state->state[j + 1][i - 1]
+                && state->state[j][i] == state->state[j + 2][i - 2]
+                && state->state[j][i] == state->state[j + 3][i - 3] ) {
+
+                return state->state[j][i];
+            }
+        }
+    }
+
+    return -1;
 }
 
 /*
@@ -79,11 +156,10 @@ bool check_for_win( board* state, bool side ) {
     Returns a score for the yellow (negative means red is winning)
 */
 int_fast64_t evaluate( board* state ) {
-    int_fast64_t result = 0;
     int_fast64_t yellow = 0;
     int_fast64_t red = 0;
 
-    // for three in a row +200
+    // three in a row
     bool check = true;
     for ( int_fast8_t i = 2; i < 4; i++ ) {
         for ( int_fast8_t j = 2; j < 5; j++ ) {
@@ -95,50 +171,45 @@ int_fast64_t evaluate( board* state ) {
             check = false;
         }
         if ( check ) {
-            if ( state->side ) yellow += 1000;
-            else               red += 1000;
+            if ( state->side ) red += 33;
+            else               yellow += 33;
             break;
         }
     }
 
-    // for four in a row +10000
-
-    // dominating the center
-    for ( int_fast8_t i = 0; i < 6; i++ ) {
-        if ( *state->state[3] == 1 )      red += 50;
-        else if ( *state->state[3] == 0 ) yellow += 50;
-
-        if ( *state->state[4] == 1 )      red += 25;
-        else if ( *state->state[4] == 0 ) yellow += 25;
-
-        if ( *state->state[2] == 1 )      red += 25;
-        else if ( *state->state[2] == 0 ) yellow += 25;
-
-        if ( *state->state[5] == 1 )      red += 10;
-        else if ( *state->state[5] == 0 ) yellow += 10;
-
-        if ( *state->state[1] == 1 )      red += 10;
-        else if ( *state->state[1] == 0 ) yellow += 10;
-    }
+    // for four in a row
+    int_fast8_t winning = check_for_win( state );
+    if ( winning == 0 ) yellow += 100;
+    else if ( winning == 1 ) red += 100;
 
     // odd even strategy
     for ( int_fast8_t i = 0; i < 3; i++ ) {
         for ( int_fast8_t j = 0; j < 7; j++ ) {
             // yellow need to fill the odd ones
             if ( state->state[j][i * 2] == 0 ) {
-                yellow += 200;
+                yellow += 10;
             }
 
-            // red need to fill the odd ones
+            // red need to fill the even ones
             if ( state->state[j][i * 2 + 1] == 1 ) {
-                red += 200;
+                red += 10;
             }
         }
     }
 
-    result = yellow - red;
+    // center and side columns are important
+    for ( int_fast8_t i = 0; i < 6; i++ ) {
+        if ( state->state[3][i] == 0 )      yellow += 5;
+        else if ( state->state[3][i] == 1 ) red += 5;
 
-    return result;
+        if ( state->state[2][i] == 0 )      yellow += 2;
+        else if ( state->state[2][i] == 1 ) red += 2;
+
+        if ( state->state[4][i] == 0 )      yellow += 2;
+        else if ( state->state[4][i] == 1 ) red += 2;
+    }
+
+    return yellow - red;
 }
 
 /*
@@ -149,7 +220,7 @@ uint_fast8_t random( uint_fast64_t seed ) {
     random8 ^= 6789912367124;
     random8 += 932094;
     random8 ^= 531323;
-    random8 -= 8123903295;
+    random8 -= 8123903293;
     return (seed ^ random8) % 256;
 }
 
@@ -163,18 +234,24 @@ uint_fast8_t random( uint_fast64_t seed ) {
 int_fast64_t analyze( board* state, uint_fast8_t sample ) {
     int_fast64_t result = 0;
     uint_fast8_t column = 0;
+    uint_fast8_t moves = 0;
     board* random_state = new board;
+    int_fast8_t win;
 
     for ( uint_fast64_t i = 0; i < pow( 2, sample ); i++ ) {
+        win = -1;
         // creating new board by copying the og
         *random_state = *state;
 
-        // making random legal moves
-        column = random( rand() ) % 7;
-        while ( random_state->make_move( column ) ) {
-            column = random( rand() ) % 7;
+        // resetting the moves
+        moves = 0;
 
-            // check if it's winning
+        // randomizing the tree
+        while ( win == -1 && moves < 42 ) {
+            // makin legal moves
+            column = random( rand() ) % 7;
+            random_state->force_move( column );
+            moves++;
         }
 
         // evaluate and add to the overall score
@@ -183,11 +260,9 @@ int_fast64_t analyze( board* state, uint_fast8_t sample ) {
 
     // remember to free up the memory, kids!
     delete random_state;
-    cout << ":" << result << "   ";
+
     // take average
     result >>= sample;
-
-    cout << result << endl;
 
     // return
     return result;
@@ -213,9 +288,11 @@ int_fast8_t search( board* state ) {
 
             // analyzing the move
             if ( state->side )
-                scores[i] = -analyze( new_state, 12 );
+                scores[i] = -analyze( new_state, 16 );
             else 
-                scores[i] = analyze( new_state, 12 );
+                scores[i] = analyze( new_state, 16 );
+
+            //cout << " Column " << int( i + 1 ) << ": " << scores[i] << endl;
 
             // searching for the best move
             if ( scores[i] > best_score ) {
@@ -237,15 +314,37 @@ int_fast8_t search( board* state ) {
 
 int main() {
     srand( time( NULL ) );
-
     board state;
 
-    while ( 1 ) {
-        state.make_move( search( &state ) );
-        state.display();
-        cout << endl;
-        system( "pause > nul" );
-    }
+    string input;
+    cout << " Connect 4 engine \n";
+    cout << " #     Strongin     #\n";
+    cout << " by Krzysztof Luczka,\n\n";
+    cout << " license and source code on\n";
+    cout << " https://github.com/krisluczka/Strongin\n\n";
+    cout << " Y - for yellow (first), R - red (second) >> "; cin >> input;
 
     state.display();
+
+    int column = 0;
+    int move = 0;
+    if ( input != "Y" && input != "y" ) {
+        move = search( &state );
+        cout << " Strongin made move " << move + 1 << "\n\n";
+        state.make_move( move );
+        state.display();
+    }
+    while ( check_for_win( &state ) == -1 ) {
+        do {
+            cout << endl;
+            cout << " Type a column index >> "; cin >> column;
+            if ( column < 1 ) column = 1;
+            if ( column > 7 ) column = 7;
+        } while ( !state.make_move( column - 1 ) );
+        state.display();
+        move = search( &state );
+        cout << " Strongin made move " << move + 1 << "\n\n";
+        state.make_move( move );
+        state.display();
+    }
 }
